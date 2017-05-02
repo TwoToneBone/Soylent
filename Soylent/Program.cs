126f55a01539abd72287bc8a418ef31253f4b791
Erik <erik.ra@gmail.com>
2017-05-03 00:02:43 +0200

    Add project files.

---
+++ Soylent/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soylent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> outputs = new List<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int calories = int.Parse(Console.ReadLine());
+
+                if (calories % 400 == 0)
+                {
+                    outputs.Add((calories / 400).ToString());
+                }
+                else
+                {
+                    outputs.Add((((400 - (calories % 400)) + calories) / 400).ToString());
+                }
+            }
+
+            foreach (var i in outputs)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
